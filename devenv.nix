bb3bd7b65204ac9da43acfa5c6be4e2f095daa54
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-09-28 22:39:43 +0200

    fix sketchybar, add docs and devenv

---
+++ devenv.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+let
+  plist_file = "~/Library/LaunchAgents/org.nixos.sketchybar.plist";
+in
+{
+  scripts.load.exec = "launchctl load -w ${plist_file}";
+  scripts.unload.exec = "launchctl unload -w ${plist_file}";
+  scripts.start = {
+    exec = ''
+      content=$(cat <<END
+      package.cpath = package.cpath .. ";${pkgs.sbarlua}/lib/lua/${pkgs.lua54Packages.lua.luaversion}/?.so"
+      sbar = require("sketchybar")
+
+      sbar.begin_config()
+      require("init")
+      sbar.hotload(true)
+      sbar.end_config()
+      sbar.event_loop()
+      END
+      )
+      echo "$content" > "${config.env.DEVENV_ROOT}/sketchybarrc"
+    '';
+    package = pkgs.lua5_4;
+  };
+
+}
