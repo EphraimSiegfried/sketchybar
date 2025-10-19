7d59598c17b4a9fc338a43654af1dabdadd31d44
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-10-19 18:15:00 +0200

    disable stats

--- items/control_center/init.lua
+++ items/control_center/init.lua
@@ -5,18 +5,18 @@ local volume = require("items.control_center.volume")
 local battery = require("items.control_center.battery")
 local wifi = require("items.control_center.wifi")
 -- local bluetooth = require("items.control_center.bluetooth")
-local cpu = require("items.control_center.cpu")
-local ram = require("items.control_center.ram")
-local disk = require("items.control_center.disk")
+-- local cpu = require("items.control_center.cpu")
+-- local ram = require("items.control_center.ram")
+-- local disk = require("items.control_center.disk")
 
 local items = {
   wifi.name,
   -- bluetooth.name,
   battery.name,
   volume.icon.name,
-  cpu.name,
-  ram.name,
-  disk.name,
+  -- cpu.name,
+  -- ram.name,
+  -- disk.name,
 }
 
 sbar.add("bracket", items, {
