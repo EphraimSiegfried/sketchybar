48907ef21db8ad51967a7735d44ef26a1d4f1870
Ephraim Siegfried <ephraim.siegfried@proton.me>
2026-04-22 08:48:15 +0200

    fix devenv script

--- devenv.nix
+++ devenv.nix
@@ -11,25 +11,22 @@ in
 {
   packages = [
     inputs.custom.packages.${pkgs.system}.sketchybar-system-stats
+    pkgs.wttrbar
   ];
   scripts.load.exec = "launchctl load -w ${plist_file}";
   scripts.unload.exec = "launchctl unload -w ${plist_file}";
-  scripts.start = {
-    exec = ''
-      content=$(cat <<END
-      package.cpath = package.cpath .. ";${pkgs.sbarlua}/lib/lua/${pkgs.lua54Packages.lua.luaversion}/?.so"
-      sbar = require("sketchybar")
+  scripts.start.exec = ''
+    cat > "${config.env.DEVENV_ROOT}/sketchybarrc" <<'END'
+    #!${pkgs.lua5_4}/bin/lua
+    package.cpath = package.cpath .. ";${pkgs.sbarlua}/lib/lua/${pkgs.lua54Packages.lua.luaversion}/?.so"
+    sbar = require("sketchybar")
 
-      sbar.begin_config()
-      require("init")
-      sbar.hotload(true)
-      sbar.end_config()
-      sbar.event_loop()
-      END
-      )
-      echo "$content" > "${config.env.DEVENV_ROOT}/sketchybarrc"
-    '';
-    package = pkgs.lua5_4;
-  };
+    sbar.begin_config()
+    require("init")
+    sbar.hotload(true)
+    sbar.end_config()
+    sbar.event_loop()
+    END
+  '';
 
 }
